8e8121f68a94137ae09327d74b7b80ed5be9690c
selome <selomegech@gmail.com>
2024-07-04 15:46:41 +0300

    first commit

---
+++ B_Festive_Matrix.py
@@ -0,0 +1,28 @@
+t= int(input())
+arr=[]
+for i in range(t):
+    arr.append(list(map(int, input().split())))
+
+n = int(t-1/2)
+elements= []
+indexes=[]
+
+for i in range(t):
+    for j in range(t):
+        if i == j or i+j == t-1:
+
+            if [i,j] not in indexes:
+                indexes.append([i,j])
+                elements.append(arr[i][j])
+        if  i == t//2 or j == t//2 :
+             if [i,j] not in indexes:
+                indexes.append([i,j])
+                elements.append(arr[i][j])
+
+print(sum(elements))
+
+
+
+
+
+    
